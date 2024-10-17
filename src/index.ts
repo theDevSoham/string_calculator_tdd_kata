88ec4f253e1f0b5fc78c2d41ef6e98958aecb21b
Soham Das <publicsoham.24@gmail.com>
2024-10-17 14:04:56 +0530

    tdd(green phase): correction of function to handle string elements with length more than 1

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,21 @@
  * 1. Skeleton function to be accessible through test
  * 2. Correction: numbers = "" i.e string length is 0
  * 3. Correction: numbers = "1" i.e string length is 1 should return the number
+ * 4. Correction: numbers = "1,5" i.e return should be 6
  */
 
 export function add(numbers: string): number {
-    // for length of string is 1
-    if (numbers.length === 1) return Number(numbers);
-
     // for length of string is 0
     if (numbers.length === 0) return 0;
+    // for length of string is 1
+    else if (numbers.length === 1) return Number(numbers);
+    // for string length greater than 1
+    else {
+        // extract all numbers from string with split and create a numbers array
+        const numArray = numbers.split(",").map((str) => Number(str));
+		// iterate over the array to get the addition of all the numbers
+        return numArray.reduce((acc, curr) => acc + curr, 0);
+    }
 
-    // default return 0
-    return 0;
+	// removed default return as it's not needed anymore
 }
