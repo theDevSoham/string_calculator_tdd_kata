939bb7ab09b9760136d1000a4dfbadfcd783d482
Soham Das <publicsoham.24@gmail.com>
2024-10-17 22:51:18 +0530

    tdd(green phase): updated code for custom delimeter support

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import Decimal from "decimal.js";
  * 4. Correction: numbers = "1,5" i.e return should be 6
  * 5. Correction: numbers = "0.1, 0.2, 0.3,... upto 1000" i.e very large amount of data should return 100
  * 6. Correction: numbers = "1\n2,3" i.e detect new line delimiter
+ * 7. Correction: numbers = "//;\n1;2" i.e override ',' with ;
  */
 
 export function add(numbers: string): number {
@@ -17,10 +18,29 @@ export function add(numbers: string): number {
     else if (numbers.length === 1) return Number(numbers);
     // for string length greater than 1
     else {
+        // save default separator
+        let separator = ",";
+
+        // extract separator
+        if (numbers.startsWith("//")) separator = numbers.slice(2, 3);
+
+        /**
+         * numbers having custom separator will strip the numbers string from delimiter implementation
+         * numbers that doesn't have custom delimiter will be assigned without any operation
+         */
+        const formattedNumbers = numbers.startsWith("//")
+            ? numbers.slice(3)
+            : numbers;
+
+        // define custom regexp for custom separator
+        const regexp: RegExp = new RegExp(`[\\n${separator}]+`, "g");
+
         // extract all numbers from string with split and create a numbers array
-        const numArray = numbers
-            .split(/[\n, ]+/) // replace ',' with regular expression, split by ',', '\n' or ' '
-            .map((str) => new Decimal(str.trim()));
+        const numArray = formattedNumbers
+            .split(regexp) // replace with dynamic regexp
+            // added str length check as empty str cannot be handled by decimal.js
+            .map((str) => new Decimal(str.length > 0 ? str.trim() : 0))
+            .filter((num) => !num.isNaN());
 
         // iterate over the array to get the addition of all the numbers
         const result = numArray.reduce(
