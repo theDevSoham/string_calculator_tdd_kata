7a512e9c67f8b1060e8163ff3af0d2b6810817b7
Soham Das <publicsoham.24@gmail.com>
2024-10-17 13:35:21 +0530

    tdd(red phase): failed test for only one input to return the same number

--- src/__tests__/str_calc_tdd.test.ts
+++ src/__tests__/str_calc_tdd.test.ts
@@ -3,3 +3,7 @@ import { add } from "..";
 test("empty string to return 0", () => {
     expect(add("")).toBe(0);
 });
+
+test("string with length 1 returns the number", () => {
+    expect(add("1")).toBe(1);
+});
