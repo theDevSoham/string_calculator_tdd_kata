abd6169e5b5892f4f4ba7e9808cf3229232caee7
Soham Das <publicsoham.24@gmail.com>
2024-10-17 13:30:20 +0530

    tdd(green phase): correction of function to support empty string input

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Test cases to pass:
+ * 1. Skeleton function to be accessible through test
+ * 2. Correction: numbers = "" i.e string length is 0
+ */
+
 export function add(numbers: string) {
-    return numbers;
+    let calculatedNum = 0;
+    // correction for numbers = ""
+    return calculatedNum;
 }
