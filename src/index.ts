3bff84460afd21da03ab218412a8807960e2596c
Soham Das <publicsoham.24@gmail.com>
2024-10-17 13:43:10 +0530

    tdd(green phase): correction of function to handle string with length 1

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@
  * Test cases to pass:
  * 1. Skeleton function to be accessible through test
  * 2. Correction: numbers = "" i.e string length is 0
+ * 3. Correction: numbers = "1" i.e string length is 1 should return the number
  */
 
-export function add(numbers: string) {
-    let calculatedNum = 0;
-    // correction for numbers = ""
-    return calculatedNum;
+export function add(numbers: string): number {
+    // for length of string is 1
+    if (numbers.length === 1) return Number(numbers);
+
+    // for length of string is 0
+    if (numbers.length === 0) return 0;
+
+    // default return 0
+    return 0;
 }
