f0a239443a6b97f55b00322eab65532007fbf3fe
Soham Das <publicsoham.24@gmail.com>
2024-10-17 14:08:26 +0530

    tdd(additional test):decimal addition test successful. No need to modify function

--- src/__tests__/str_calc_tdd.test.ts
+++ src/__tests__/str_calc_tdd.test.ts
@@ -11,3 +11,7 @@ test("string with length 1 returns the number", () => {
 test("sum 1 + 5 returns 6", () => {
     expect(add("1,5")).toBe(6);
 });
+
+test("decimal addition: sum 5.12 + 6.1342 + 7.63 returns 18.8842", () => {
+    expect(add("5.12, 6.1342, 7.63")).toBe(5.12 + 6.1342 + 7.63);
+});
